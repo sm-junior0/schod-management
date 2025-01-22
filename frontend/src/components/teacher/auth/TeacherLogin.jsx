70967f956b80f9703010cfe895fcfda83a016a01
Manene <juniormanene@gmail.com>
2025-01-22 14:31:48 +0200

    link activated

--- frontend/src/components/teacher/auth/TeacherLogin.jsx
+++ frontend/src/components/teacher/auth/TeacherLogin.jsx
@@ -107,10 +107,12 @@ export const TeacherLogin = () => {
               </button>
             </div>
             <div mt-4 className="text-center">
-              <button type="button"
-              onClick={() => navigate("/")}
-              className="font-medium text-blue-600 hover:text-blue-500">
-                   Go to Home Page
+              <button
+                type="button"
+                onClick={() => navigate("/")}
+                className="font-medium text-blue-600 hover:text-blue-500"
+              >
+                Go to Home Page
               </button>
             </div>
           </form>
